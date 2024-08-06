a8c0c014b4dfb73dea4f476999f078b0b40441f1
Sudeep Nair <sudeepn15@gmail.com>
2024-08-06 09:04:51 +0530

    add TodoList class

---
+++ src/TodoList/Main.java
@@ -0,0 +1,6 @@
+package TodoList;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
--- src/TodoList/TodoList.java
+++ src/TodoList/TodoList.java
@@ -1,5 +1,44 @@
 package TodoList;
 
+import java.util.ArrayList;
+
 public class TodoList {
+
+    private ArrayList<Task> tasks;
+
+    public TodoList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        this.tasks.add(new Task(description));
+    }
+
+    public void completeTask(int index) {
+        if (index < 0 || index >= tasks.size()) {
+            System.out.println("Oops!! Invalid Task number");
+        } else {
+            this.tasks.get(index).complete();
+        }
+    }
+
+    public void removeTask(int index) {
+        if (index < 0 || index >= tasks.size()) {
+            System.out.println("Oops!! Invalid Task number");
+        } else {
+            this.tasks.remove(index);
+        }
+    }
+
+    public void displayList() {
+        if (tasks.isEmpty()) {
+            System.out.println("Your List is empty!");
+            System.out.println("Type add command to start adding tasks to your list");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println("(" + (i + 1) + ") " + tasks.get(i));
+            }
+        }
+    }
 
 }
